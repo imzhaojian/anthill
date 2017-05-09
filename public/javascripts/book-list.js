67e4579d7d53754fa206f9d1f3a9e8ee6472c767
zhaojian <278213679@qq.com>
2017-05-09 14:34:57 +0800

    添加图书列表

--- public/javascripts/book-list.js
+++ public/javascripts/book-list.js
@@ -3,14 +3,14 @@
  */
 
 var BookNode = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <li className="bookNode">
                 <div className="cover">
                     <img src={this.props.data.bookImg} />
                 </div>
                 <div className="name" title={this.props.data.bookName}>
-                    <a href={"/book/info/"+this.props.data.bookId} target="_blank">{this.props.data.bookName}</a>
+                    <a href={"/book/info/" + this.props.data.bookId} target="_blank">{this.props.data.bookName}</a>
                 </div>
                 <div className="appraise">
                     <i className="icon star"></i>
@@ -29,8 +29,8 @@ var BookNode = React.createClass({
 });
 
 var BookListContainer = React.createClass({
-    render: function() {
-        var bookNodes = this.props.data.map(function(comment) {
+    render: function () {
+        var bookNodes = this.props.data.map(function (comment) {
             return (
                 <BookNode data={comment} key={comment.bookId}></BookNode>
             );
@@ -45,18 +45,18 @@ var BookListContainer = React.createClass({
     }
 });
 
-var data=[
-    {bookId:"1",bookName:"JavaScript权威指南(第1版)",bookImg:"./images/book1.jpg",desc:"内容涵盖JavaScript语言本身1"},
-    {bookId:"2",bookName:"JavaScript权威指南(第2版)",bookImg:"./images/book2.jpg",desc:"内容涵盖JavaScript语言本身2"},
-    {bookId:"3",bookName:"JavaScript权威指南(第3版)",bookImg:"./images/book3.jpg",desc:"内容涵盖JavaScript语言本身3"},
-    {bookId:"4",bookName:"JavaScript权威指南(第4版)",bookImg:"./images/book4.jpg",desc:"内容涵盖JavaScript语言本身4"},
-    {bookId:"5",bookName:"JavaScript权威指南(第5版)",bookImg:"./images/book1.jpg",desc:"内容涵盖JavaScript语言本身5"},
-    {bookId:"6",bookName:"JavaScript权威指南(第6版)",bookImg:"./images/book2.jpg",desc:"内容涵盖JavaScript语言本身6"},
-    {bookId:"7",bookName:"JavaScript权威指南(第7版)",bookImg:"./images/book3.jpg",desc:"内容涵盖JavaScript语言本身7"},
-    {bookId:"8",bookName:"JavaScript权威指南(第8版)",bookImg:"./images/book4.jpg",desc:"内容涵盖JavaScript语言本身8"},
-    {bookId:"9",bookName:"JavaScript权威指南(第9版)",bookImg:"./images/book2.jpg",desc:"内容涵盖JavaScript语言本身9"},
-    {bookId:"10",bookName:"JavaScript权威指南(第10版)",bookImg:"./images/book3.jpg",desc:"内容涵盖JavaScript语言本身10"},
-    {bookId:"11",bookName:"JavaScript权威指南(第11版)",bookImg:"./images/book4.jpg",desc:"内容涵盖JavaScript语言本身11"}
+var data = [
+    {bookId: "1", bookName: "JavaScript权威指南(第1版)", bookImg: "./images/book1.jpg", desc: "内容涵盖JavaScript语言本身1"},
+    {bookId: "2", bookName: "JavaScript权威指南(第2版)", bookImg: "./images/book2.jpg", desc: "内容涵盖JavaScript语言本身2"},
+    {bookId: "3", bookName: "JavaScript权威指南(第3版)", bookImg: "./images/book3.jpg", desc: "内容涵盖JavaScript语言本身3"},
+    {bookId: "4", bookName: "JavaScript权威指南(第4版)", bookImg: "./images/book4.jpg", desc: "内容涵盖JavaScript语言本身4"},
+    {bookId: "5", bookName: "JavaScript权威指南(第5版)", bookImg: "./images/book1.jpg", desc: "内容涵盖JavaScript语言本身5"},
+    {bookId: "6", bookName: "JavaScript权威指南(第6版)", bookImg: "./images/book2.jpg", desc: "内容涵盖JavaScript语言本身6"},
+    {bookId: "7", bookName: "JavaScript权威指南(第7版)", bookImg: "./images/book3.jpg", desc: "内容涵盖JavaScript语言本身7"},
+    {bookId: "8", bookName: "JavaScript权威指南(第8版)", bookImg: "./images/book4.jpg", desc: "内容涵盖JavaScript语言本身8"},
+    {bookId: "9", bookName: "JavaScript权威指南(第9版)", bookImg: "./images/book2.jpg", desc: "内容涵盖JavaScript语言本身9"},
+    {bookId: "10", bookName: "JavaScript权威指南(第10版)", bookImg: "./images/book3.jpg", desc: "内容涵盖JavaScript语言本身10"},
+    {bookId: "11", bookName: "JavaScript权威指南(第11版)", bookImg: "./images/book4.jpg", desc: "内容涵盖JavaScript语言本身11"}
 ]
 
 ReactDOM.render(
@@ -64,7 +64,7 @@ ReactDOM.render(
     document.getElementById('content')
 );
 
-$(function() {
+$(function () {
     $('body').perfectScrollbar();
 
     // with vanilla JS!
